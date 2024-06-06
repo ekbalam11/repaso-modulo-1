456bab18e6366fba80b86dabddb193bd44e650ca
ekbalam11 <ekbalam11@gmail.com>
2024-06-06 19:26:46 +0200

    arrays excercise 1 done

--- algoritmia/1-remove-rotten.js
+++ algoritmia/1-remove-rotten.js
@@ -1,7 +1,15 @@
 // enunciado https://www.codewars.com/kata/557af4c6169ac832300000ba/
 
 function removeRotten(bagOfFruits) {
-    // ...
+    let freshFruits = [];
+
+    for (let fruit of bagOfFruits){
+        let cleanedFruit = fruit.replace("rotten","");
+        cleanedFruit = cleanedFruit.toLowerCase();
+        freshFruits.push(cleanedFruit)
+    }
+
+    return freshFruits;
 }
 
 console.log(removeRotten([])); // []
@@ -10,3 +18,26 @@ console.log(removeRotten(["apple", "banana", "kiwi", "melone", "orange"])); // [
 
 console.log(removeRotten(["apple", "rottenBanana", "apple"])); //["apple","banana","apple"] 
 
+console.log(removeRotten(["rotten", "is rotten?"]));
+
+console.log(removeRotten("rottenPatata"));
+
+
+
+//Como repasar las letras de una palabra?
+
+function quitarM (palabra) {
+    let palabrasnoM = [];
+    for (let letra of palabra) {
+        let palabrassinM = letra.replace("M","");
+        palabrasnoM.push(palabrassinM)
+    }
+
+    return palabrasnoM;
+}
+
+
+console.log(quitarM([]));
+
+
+console.log(quitarM("Mamita"));
